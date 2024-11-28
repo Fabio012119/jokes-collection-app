cc140cc18a305b92736da8b83fb6c7958d0d5b9c
Fabio <fabiooosara@gmail.com>
2024-11-28 15:11:21 +0200

    initialize

---
+++ src/components/Main.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>HELLO WORLD</div>
+</template>
+<script>
+export default {
+  name: "MainComponent",
+};
+</script>
+
+<style>
+h2 {
+  color: blue;
+}
+</style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,17 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App" />
+    <MainComponent />
   </div>
 </template>
 
 <script lang="ts">
 import { defineComponent } from "vue";
-import HelloWorld from "@/components/HelloWorld.vue"; // @ is an alias to /src
+import MainComponent from "@/components/Main.vue";
 
 export default defineComponent({
   name: "HomeView",
   components: {
-    HelloWorld,
+    MainComponent,
   },
 });
 </script>
