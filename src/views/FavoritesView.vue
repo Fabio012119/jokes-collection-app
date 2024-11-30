c78d6bf71e71d4cbc58b08c98b7eaa3cb8f76e97
Fabio <fabiooosara@gmail.com>
2024-12-01 01:27:59 +0200

    add search bar with total bookings

--- src/views/FavoritesView.vue
+++ src/views/FavoritesView.vue
@@ -1,16 +1,42 @@
 <template>
+  <div class="controls p-4 flex flex-col items-center">
+    <input
+      type="text"
+      v-model="searchQuery"
+      placeholder="Search jokes..."
+      class="mb-4 px-4 py-2 border rounded w-full max-w-lg"
+    />
+  </div>
+
+  <div class="text-center mb-4">
+    <p>Total Jokes Saved: {{ totalJokes }}</p>
+  </div>
+
   <jokes-component
     :isFavorites="true"
-    :jokes="jokes"
+    :jokes="filteredJokes"
     :loading="false"
     :error="error"
+    @updateJokeRating="handleJokeRatingUpdate"
   />
 </template>
 
 <script setup>
 import JokesComponent from "@/components/JokesComponent.vue";
+import { ref } from "vue";
 
 const jokes = JSON.parse(localStorage.getItem("savedJokes") || "[]");
+const searchQuery = ref("");
 
-const error = jokes.length === 0 ? "Add some favorite jokes" : null;
+const filteredJokes = () => {
+  let result = jokes;
+
+  if (searchQuery.value) {
+    result = result.filter((joke) =>
+      joke.setup.toLowerCase().includes(searchQuery.value.toLowerCase())
+    );
+  }
+
+  return result;
+};
 </script>
